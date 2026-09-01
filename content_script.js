ab15592c44a9d95ddd9a1ad58e1f93dfd8eb875a
Joe Capezzuto <joseph.capezzuto@gmail.com>
2026-09-01 10:53:53 -0700

    Retrive API url while using browser history to navigate

--- content_script.js
+++ content_script.js
@@ -1,30 +1,40 @@
 (function () {
   async function init() {
     try {
-      const apiUrl = await new Promise((resolve, reject) => {
-        const metadataContainer = document.querySelector('.m-technical-data .m-technical-data__metadata-fields');
-        // metadata children seem to be populated later in page load, so wait for API link to become available
-        const observer = new MutationObserver((mutations) => {
-          let apiLinkElem;
-          for (const mutation of mutations) {
-            const firstAddedNode = mutation.addedNodes[0];
-            if (firstAddedNode?.classList?.contains('m-technical-data__metadata-field')) {
-              // get link to API without targeting link to Query Builder
-              apiLinkElem = firstAddedNode.querySelector(
-                '.m-technical-data__metadata-field a[href^="https://data.getty.edu/museum/collection/"]:not([href*=sparql])',
-              );
-              if (apiLinkElem) {
-                observer.disconnect();
-                break;
+      // on new page loads, the "APIs & other identifiers" section is populated slightly later than other parts of the page
+      // MutationObserver tracks nodes being added to wait for this data to become available
+      // However, if page is cached, particularly when using browser history (i.e., Back and Forward buttons),
+      // the section in question might already be populated and the MutationObserver logic will not run
+      let apiUrl;
+      let apiLinkElem = document.querySelector(
+        '.m-technical-data__metadata-field a[href^="https://data.getty.edu/museum/collection/"]:not([href*=sparql])',
+      );
+      if (apiLinkElem) {
+        apiUrl = apiLinkElem.href;
+      } else {
+        apiUrl = await new Promise((resolve, reject) => {
+          const metadataContainer = document.querySelector('.m-technical-data .m-technical-data__metadata-fields');
+          const observer = new MutationObserver((mutations) => {
+            let apiLinkElem;
+            for (const mutation of mutations) {
+              const firstAddedNode = mutation.addedNodes[0];
+              if (firstAddedNode?.classList?.contains('m-technical-data__metadata-field')) {
+                // get link to API without targeting link to Query Builder
+                apiLinkElem = firstAddedNode.querySelector(
+                  '.m-technical-data__metadata-field a[href^="https://data.getty.edu/museum/collection/"]:not([href*=sparql])',
+                );
+                if (apiLinkElem) {
+                  observer.disconnect();
+                  break;
+                }
               }
             }
-          }
-          apiLinkElem ? resolve(apiLinkElem.href) : reject(new Error('No API link element detected'));
+            apiLinkElem ? resolve(apiLinkElem.href) : reject(new Error('No API link element detected'));
+          });
+          observer.observe(metadataContainer, { childList: true, attributes: false });
         });
-        observer.observe(metadataContainer, { childList: true, attributes: false });
-      });
+      }
 
-      // const apiUrl = apiElem.href;
       await browser.storage.local.set({ apiUrl });
 
       // document does not completely refresh, so DOMContentLoaded event will not
@@ -55,7 +65,6 @@
 
   if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', () => {
-      console.log('loaded');
       init();
     });
   } else {
